070a7bc76faea378d2ba071ca6edb5cb7695abf8
freyta <freyta@gmail.com>
2018-03-22 08:36:31 +1100

    Update main.py
    Download multiple files now

--- main.py
+++ main.py
@@ -7,7 +7,17 @@ from datetime import datetime, timedelta
 import json
 import os.path
 import time
+import argparse
 import wwe
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-t","--type", help="download a single episode or year [options: \"year\" or \"single\"]", default="single")
+args = parser.parse_args()
+if (args.type == "year"):
+    episodes_wanted = args.type
+else:
+    episodes_wanted = "single"
 
 # Set some basic details and login
 email = 'YOUR EMAIL HERE'
@@ -15,30 +25,39 @@ password = 'YOUR PASSWORD HERE'
 # Available options are: 4500, 3000, 2400, 1800, 1200, 800
 quality = '4500'
 
+# Login to the network
 wwe_network = wwe.Network(email, password)
 wwe_network.login()
 
 # Get the show the user wants
-show_link = wwe.get_user_input_show()
+show_link = getattr(wwe, 'get_user_input_show_' + episodes_wanted)()
+
 
 # Open the link for the show
 json_response = urllib.urlopen(show_link[0])
 show_link_json = json.loads(json_response.read())
 
-# Generate a Kodi NFO file for the show/PPV/collection
-get_nfo = getattr(wwe, 'get_'+show_link[1]+'_nfo')(show_link[0])
 
-
-# Gets the first show from the users selection
-
-show_video_id = show_link_json['list'][1]['itemTags']['media_playback_id']
-
-# Make a name for the video from the headline + date
-video_name = show_link_json['list'][1]['headline'] + '-' + show_link_json['list'][1]['itemTags']['event_date'][0].split('T')[0]
-video_name = wwe.clean_name(video_name)
-print video_name
-# Get the video
-wwe_network.get_video_url(show_video_id, video_name, '4500')
-
-# And finally, logout
+# If it isn't a collection, show available episodes to download
+if(not show_link[1]=="collection"):
+    video_ids = wwe.download_multiple(show_link[0])
+    #video_ids = [u'1886974483'] (url = http://network.wwe.com/video/v1886974483)
+    
+    # If it is a TV Show
+    if show_link[1]=="tvshow":
+        for(videoid, showname, date) in zip(video_ids[0], video_ids[1], video_ids[2]):
+            # Create an useable name
+            showname = wwe.clean_name(showname)
+            showname = showname+".-."+date
+            # Get the video
+            wwe_network.get_video_url(videoid, showname, quality)
+    else:
+        for(videoid, showname) in zip(video_ids[0], video_ids[1]):
+            # Create an useable name
+            showname = wwe.clean_name(showname)
+            showname = showname+".-."+date
+            # Get the video
+            wwe_network.get_video_url(videoid, showname, quality)
+            
+# Finally logout
 wwe_network.logout()
