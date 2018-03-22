af242797c7d277a30e184e0a98a30aa5b4977c7b
freyta <freyta@gmail.com>
2018-03-22 14:28:45 +1100

    Update main.py
    Added option to download individual shows too

--- main.py
+++ main.py
@@ -25,7 +25,6 @@ password = 'YOUR PASSWORD HERE'
 # Available options are: 4500, 3000, 2400, 1800, 1200, 800
 quality = '4500'
 
-# Login to the network
 wwe_network = wwe.Network(email, password)
 wwe_network.login()
 
@@ -33,31 +32,59 @@ wwe_network.login()
 show_link = getattr(wwe, 'get_user_input_show_' + episodes_wanted)()
 
 
-# Open the link for the show
-json_response = urllib.urlopen(show_link[0])
-show_link_json = json.loads(json_response.read())
+if not show_link[1]=="single_tvshow":
+    # Open the link for the show
+    json_response = urllib.urlopen(show_link[0])
+    show_link_json = json.loads(json_response.read())
 
 
 # If it isn't a collection, show available episodes to download
 if(not show_link[1]=="collection"):
-    video_ids = wwe.download_multiple(show_link[0])
-    #video_ids = [u'1886974483'] (url = http://network.wwe.com/video/v1886974483)
-    
-    # If it is a TV Show
-    if show_link[1]=="tvshow":
-        for(videoid, showname, date) in zip(video_ids[0], video_ids[1], video_ids[2]):
-            # Create an useable name
-            showname = wwe.clean_name(showname)
-            showname = showname+".-."+date
-            # Get the video
-            wwe_network.get_video_url(videoid, showname, quality)
+    if(episodes_wanted == "year"):
+        video_ids = wwe.download_multiple(show_link[0])
+        # If it is a TV Show
+        if show_link[1]=="tvshow":
+            for(videoid, showname, date) in zip(video_ids[0], video_ids[1], video_ids[2]):
+                # Clean up the video name a bit
+                showname = wwe.clean_name(showname)
+                showname = showname+"."+date
+                # Get the video
+                wwe_network.get_video_url(videoid, showname, quality)
+        # We want to download a PPV
+        else:
+            for(videoid, showname, year) in zip(video_ids[0], video_ids[1], video_ids[2]):
+                # Clean up the video name a bit
+                showname = wwe.clean_name(showname + "." + year.split('-')[0])
+                
+                # Get the video
+                wwe_network.get_video_url(videoid, showname, quality)
+    # We just want to download a single episode
     else:
-        for(videoid, showname) in zip(video_ids[0], video_ids[1]):
-            # Create an useable name
-            showname = wwe.clean_name(showname)
-            showname = showname+".-."+date
+        # If it is a TV Show
+        if show_link[1]=="single_tvshow":
+            # Clean up the video name a bit
+            showname = wwe.clean_name(show_link[2])
+            showname = showname + "." + show_link[3]
             # Get the video
-            wwe_network.get_video_url(videoid, showname, quality)
-            
-# Finally logout
-wwe_network.logout()
+            wwe_network.get_video_url(show_link[0], showname, quality)
+        # We want to download a PPV
+        else:
+            video_ids = wwe.download_multiple(show_link[0])
+            for(videoid, showname, year) in zip(video_ids[0], video_ids[1], video_ids[2]):
+                # Clean up the video name a bit
+                showname = wwe.clean_name(showname + "." + year.split('-')[0])
+                # Get the video
+                wwe_network.get_video_url(videoid, showname, quality)
+# We must be a collection
+else:
+    video_ids = wwe.download_collection(show_link[0])
+    counter = 0
+    for(videoid, showname) in zip(video_ids[0], video_ids[1]):
+        # Clean up the video name a bit
+        showname = wwe.clean_name(showname)
+        showname = str(counter) + "." + showname
+        
+        # Get the video
+        wwe_network.get_video_url(videoid, showname, quality)
+        counter = counter + 1
+wwe.logout()
